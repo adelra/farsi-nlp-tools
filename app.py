b230db654bb571fe5f49ec1af016a311bdb3aef5
Adel Rahimi <rahimi.adel@gmail.com>
2016-12-23 22:06:38 +0330

    Update app.py

--- app.py
+++ app.py
@@ -13,13 +13,22 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/result.html', methods=['POST'])
+@app.route('/ResultToken.html', methods=['POST'])
 def tokenizer():
-    text = request.form[u'text']
+    text = request.form[u'TokenText']
     result = word_tokenize(text)
     string = str(result)
     return string
 
+@app.route('/ResultNorm.html', methods=['POST'])
+def normalize():
+    NormText = request.form[u'NormText']
+    normalizer = Normalizer()
+    ResultNorm = normalizer.normalize(NormText)
+    StringNorm = str(ResultNorm)
+    return StringNorm
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
